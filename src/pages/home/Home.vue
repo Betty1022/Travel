503e3861c5c9329d19718d2d44b94fcf444fb8c7
Betty1022 <1129364722@qq.com>
2018-07-04 17:41:46 +0800

    add icons and recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,21 +1,61 @@
 <template>
 	<div>
-		<home-header></home-header>
-		<home-swiper></home-swiper>
-		
+		<home-header :city="city"></home-header>
+		<home-swiper :list="swiperList"></home-swiper>
+		<home-icons :list="iconList"></home-icons>
+		<home-recommend :list="recomendList"></home-recommend>
+		<home-weekend :list="weekendList"></home-weekend>
 	</div>
 </template>
 <script>
 	import HomeHeader from './components/Header'
 	import HomeSwiper from './components/Swiper'
+	import HomeIcons from './components/Icons'
+	import HomeRecommend from './components/Recommend'
+	import HomeWeekend from './components/Weekend'
+	import axios from 'axios'
 	export default{
 		name:'Home',
 		components:{
 			HomeHeader:HomeHeader,
-			HomeSwiper:HomeSwiper   //键值一样时，可以只写一个HomeSwiper
-		}
+			HomeSwiper:HomeSwiper,//键值一样时，可以只写一个HomeSwiper
+			HomeIcons:HomeIcons,
+			HomeRecommend:HomeRecommend,
+			HomeWeekend:HomeWeekend
+		},
+		data (){
+			return{
+				city:'',
+				swiperList:[],
+				iconList:[],
+				recomendList:[],
+				weekendList:[]
+
+			}
+		},
+		methods:{
+			getHomeInfo (){
+				axios.get('/api/index.json')
+				.then(this.getHomeInfoSucc)
+			},
+			getHomeInfoSucc (res){
+				res = res.data
+				if(res.ret && res.data){
+					const data = res.data
+					this.city = data.city
+					this.swiperList = data.swiperList
+					this.iconList = data.iconList
+					this.recomendList = data.recomendList
+					this.weekendList = data.weekendList
+				}
+				console.log(res)
+			}
+		},
+		mounted (){
+			this.getHomeInfo()
+		},
+
 	}
 </script>
 <style>
-	
 </style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="wrapper">
-		<swiper :options="swiperOption">
+		<swiper :options="swiperOption" v-if="showSwiper">
 		    <!-- slides -->
-		    <swiper-slide v-for="item of swiperList" :key="item.id">
+		    <swiper-slide v-for="item of list" :key="item.id">
 		    	<img class="swiper-img" :src="item.imgUrl" />
 		    </swiper-slide>
 		    <!-- Optional controls -->
@@ -13,13 +13,17 @@
 <script>
 	export default{
 		name:'HomeSwiper',
+		props: {
+			list:Array
+		},
 		data (){
 			return{
-				swiperOption:{
+				swiperOption: {
 					pagination:'.swiper-pagination',
 					loop:true
 				},
-				swiperList:[{
+
+				/*swiperList:[{
 					id:'0001',
 					imgUrl:'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
 				},{
@@ -28,7 +32,13 @@
 				},{
 					id:'0003',
 					imgUrl:'http://img1.qunarzz.com/piao/fusion/1806/de/df09fa61aed2d502.jpg_750x200_67472739.jpg'
-				}]
+				}]*/
+			}
+
+		},
+		computed:{
+			showSwiper (){
+				return this.list.length
 			}
 		}
 	}
