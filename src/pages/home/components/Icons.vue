44ee9fb72e0d8f050ed8f678a3fd542d618ecefa
Betty1022 <1129364722@qq.com>
2018-07-04 17:44:40 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,165 @@
+<template>
+	<div class="icons">
+		<swiper :options="swiperOption">
+			<swiper-slide v-for="(page,index) of pages" :key="index">
+				<div class="icon" v-for="item of page" :key="item.id">
+					<div class="icon-img">
+						<img class="icon-image-content" :src="item.imgUrl">
+					</div>
+					<p class="icon-desc">{{item.desc}}</p>
+				</div>
+			</swiper-slide>
+			<!--<swiper-slide>
+				<div class="icon">
+					<div class="icon-img">
+						<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png">
+					</div>
+					<p class="icon-desc">必游榜单</p>
+				</div>
+			</swiper-slide>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png">
+				</div>
+				<p class="icon-desc">游乐场</p>
+			</div>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png">
+				</div>
+				<p class="icon-desc">夏日玩水</p>
+			</div>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png">
+				</div>
+				<p class="icon-desc">动植物园</p>
+			</div>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png">
+				</div>
+				<p class="icon-desc">一日游</p>
+			</div>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/dd/cce1231836f10a02.png">
+				</div>
+				<p class="icon-desc">周边游</p>
+			</div>
+			<div class="icon">
+				<div class="icon-img">
+					<img class="icon-image-content" src="http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png">
+				</div>
+				<p class="icon-desc">自然风光</p>
+			</div>-->
+		</swiper>
+	</div>
+</template>
+<script>
+	export default{
+		name:"HomeIcon",
+		props:{
+			list:Array
+		},
+		data (){
+			return{
+				swiperOption: {
+						autoplay:false
+					},
+				/*iconList:[{
+					id:"0001",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:"景点门票"
+				},{
+					id:"0002",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+					desc:"必游榜单"
+				},{
+					id:"0003",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png",
+					desc:"游乐场"
+				},{
+					id:"0004",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png",
+					desc:"夏日玩水"
+				},{
+					id:"0005",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+					desc:"动植物园"
+				},{
+					id:"0006",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+					desc:"一日游"
+				},{
+					id:"0007",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/dd/cce1231836f10a02.png",
+					desc:"周边游"
+				},{
+					id:"0008",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png",
+					desc:"自然风光"
+				},{
+					id:"0009",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png",
+					desc:"演出"
+				},{
+					id:"0010",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png",
+					desc:"全部玩乐"
+				}]*/
+			}
+		},
+		computed:{
+			pages (){
+				const pages = []
+				this.list.forEach((item,index) => {
+					const page = Math.floor(index/8)
+					if(!pages[page]){
+						pages[page] = []
+					}
+					pages[page].push(item)
+				})
+				return pages;
+			}
+		}
+	}
+</script>
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	@import '~styles/mixins.styl'
+	.icons >>> .swiper-container
+		height:0
+		padding-bottom:50%
+	.icons
+		margin-top:.1rem
+	.icon
+		position:relative
+		overflow:hidden
+		width:25%
+		height:0
+		float:left
+		padding-bottom:25%
+		.icon-img
+			position:absolute
+			top:0
+			left:0
+			right:0
+			bottom:.44rem
+			box-sizing:border-box
+			padding:.1rem
+			.icon-image-content
+				display:block
+				margin:0 auto
+				height:100%
+		.icon-desc
+			position:absolute
+			left:0
+			right:0
+			bottom:0
+			line-height:.44rem
+			height:.44rem
+			text-align:center
+			color:$darkTextColor
+			ellipsis()
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<div class="title">
+			周末去哪儿
+		</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of list" :key="item.id">
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl">
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default{
+		name:"HomeWeekend",
+		props:{
+			list:Array
+		},
+		/*data (){
+				return{
+					recommendList:[{
+						id:"0001",
+						imgUrl:"http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg",
+						title:"武汉必游",
+						desc:"到武汉感受神奇的热干面之都"
+					},{
+						id:"0002",
+						imgUrl:"http://img1.qunarzz.com/sight/source/1505/4f/95a94c90ef3521.jpg_r_640x214_345412f9.jpg",
+						title:"开启亲子时光",
+						desc:"这些都是宝贝们的最爱"
+					},{
+						id:"0003",
+						imgUrl:"http://img1.qunarzz.com/sight/source/1505/ce/0dd5b8b5f5fd2a.jpg_r_640x214_929b1bec.jpg",
+						title:"玩遍楚河街",
+						desc:"现代版的“清明上河图"
+					},{
+						id:"0004",
+						imgUrl:"http://img1.qunarzz.com/sight/source/1507/e0/4aae49007c8a64.jpg_r_640x214_65a42783.jpg",
+						title:"周末出游乐翻天",
+						desc:"这是当地人最爱的休闲时光"
+					}]
+				}
+			}*/
+	}
+</script>
+<style lang="stylus" scoped>
+	@import '~styles/mixins.styl'
+	.title
+		line-height:.8rem
+		background:#eee
+		text-indent:.2rem
+	.item-img-wrapper
+		height:0
+		ovreflow:hidden
+		padding-bottom:37.09%
+	.item-img
+		width:100%
+	.item-info
+		padding:.1rem
+		.item-title
+			line-height:.54rem
+			font-size:.28rem
+			ellipsis()
+		.item-desc
+			line-height:.4rem
+			color:#ccc
+			ellipsis()
+</style>
